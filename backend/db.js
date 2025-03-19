a5aba83527021191a836e179826efc200120904f
skrijwan <skrijwan329@gmail.com>
2025-03-20 01:17:34 +0530

    enhance database connection with new options and error

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 const mongoose= require('mongoose')
 const connectserver =async()=>{
     try {
-        await mongoose.connect(process.env.mongoURI)
+        await mongoose.connect(process.env.mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("The server is run.")
         
     } catch (error) {
         console.log(error)
-        
+        process.exit(1);
     }
 }
 module.exports= connectserver
